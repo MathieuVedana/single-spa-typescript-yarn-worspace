b1391246a1cd6287dd043ce4341d1a6409353ca2
Mathieu Mathias <mathieu.mathias@vedana.com>
2023-04-09 18:24:33 +0200

    add ts-loader to emit declaration files in webpack memory

--- packages/utils1/webpack.config.js
+++ packages/utils1/webpack.config.js
@@ -1,5 +1,14 @@
-const { merge } = require("webpack-merge");
+const { mergeWithRules } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
+
+const merge = mergeWithRules({
+  module: {
+    rules: {
+      test: "match",
+      use: "replace",
+    },
+  },
+});
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -11,5 +20,25 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts)x?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+            },
+            {
+              loader: "ts-loader",
+              options: {
+                projectReferences: true,
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
   });
 };
